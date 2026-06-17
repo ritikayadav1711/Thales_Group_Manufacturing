50b22733e5c968ce8c0efdd6a24f1dc08a92aee9
ritikayadav1711 <ritikayadav1301@gmail.com>
2026-06-17 16:48:54 +0530

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 st.title("Thales Smart Factory Dashboard")
 
-df = pd.read_csv("../data/Thales_Group_Manufacturing.csv")
+df = pd.read_csv("Thales_Group_Manufacturing.csv")
 
 st.dataframe(df.head())
